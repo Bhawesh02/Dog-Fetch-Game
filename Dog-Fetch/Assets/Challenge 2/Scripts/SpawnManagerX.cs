54f417ae6686ac90ee71b7e2ed64e32574e18677
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-09-01 01:19:55 +0530

    Random Spawn time for ball

--- Dog-Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Dog-Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
@@ -11,23 +12,26 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        Debug.Log("BallSpaw");
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
         int ballIndex = Random.Range(0, ballPrefabs.Length);
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        Invoke("SpawnRandomBall", Random.Range(3,5));
     }
 
 }
