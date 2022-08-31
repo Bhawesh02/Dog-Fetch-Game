1c7ad75deb09f5410a4277801732d6de65cd754c
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-09-01 01:20:40 +0530

    No need start delay

--- Dog-Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Dog-Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,7 +11,6 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
     private float spawnInterval = 1.0f;
 
     // Start is called before the first frame update
