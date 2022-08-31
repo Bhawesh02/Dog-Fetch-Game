aeecb9e2c73075f60028807a84d1fddbddaa8a38
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-09-01 01:26:17 +0530

    Doog can only be spawned after 1 sec

--- Dog-Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Dog-Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    public float dogDelay = 1.0f;
+    private float counter = 0;
     // Update is called once per frame
     void Update()
     {
+        counter += Time.deltaTime;
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if(counter > dogDelay)
+            {
+                SpawnDog();
+            }
+            
         }
+    }
+    void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        counter = 0;
     }
 }
